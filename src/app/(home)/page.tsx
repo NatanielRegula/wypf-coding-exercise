ab9cd50c795064bd7d9f35b2677d9da56ab07314
Nataniel Regula <nataniel.regula17@gmail.com>
2023-09-30 01:06:15 +0100

    feat: add home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,11 +1,18 @@
+import ContentWrapper from '@/global_components/content_wrapper/ContentWrapper';
 import styles from './page.module.css';
+import { ButtonLink } from '@/global_components/button/Button';
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main id="main">
-        <span>placeholder</span>
-      </main>
-    </div>
+    <main className={styles.page}>
+      <ContentWrapper fullHeight className={styles.contentWrapper}>
+        <h1>WYPF Coding Exercise</h1>
+        <p className={styles.subheading}>by Nataniel Regula</p>
+        <div className={styles.ctaWrapper}>
+          <ButtonLink href={'/users'}>Users</ButtonLink>
+          <ButtonLink href={'/albums'}>Albums</ButtonLink>
+        </div>
+      </ContentWrapper>
+    </main>
   );
 }
